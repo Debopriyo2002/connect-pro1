e356aa9aec1f9e642d83e7235ce556a7fb67f5fc
unknown <manishraj94112@gmail.com>
2023-11-09 10:03:28 +0530

    Commit for the Recommand

--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -6,25 +6,25 @@ import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 function Widgets() {
   // Define an array of news articles
   const newsArticles = [
-    { heading: "The Full Stack Junkie Is Back", subtitle: "Top news - 9099 readers" },
-    { heading: "Coronavirus: UK updates", subtitle: "Top news - 886 readers" },
-    { heading: "Bitcoin Breaks $22K", subtitle: "Crypto - 8K readers" },
-    { heading: "JavaScript Mastery", subtitle: "Code - 1.2M readers" },
-    { heading: "Tesla hits new highs", subtitle: "Cars & auto - 300 readers" },
-    { heading: "AI overtaking the World", subtitle: "Top news - 13K readers" },
-    { heading: "Apple's AirTag 2 release pushed back to 2025", subtitle: "Tech news - 1K readers" },
-    { heading: "Whats the strategy behind the acquisition of game development tools like “Twinmotion” by Epic Games?", subtitle: "Gaming news - 1.3K readers" },
-    { heading: "10 essential cybersecurity cheat sheets available for free", subtitle: "Tech news - 1.7K readers" },
-    { heading: "Top Job after BCA", subtitle: "Tech news - 7.1K readers" },
+    { heading: "Tottenham vs Chelsea: The wildest first half in Premier League history?", subtitle: "SkySport" },
+    { heading: "Chelsea want Jean-Clair Todibo to replace Thiago Silva who is set to leave at end of season - Paper Talk", subtitle: "SkySport" },
+    { heading: "Liverpool's Luis Diaz's parents kidnapped in Colombia", subtitle: "Mirror.co" },
+    { heading: "No Neymar, but Mumbai City face another stern Al Hilal test", subtitle: "T.O.I" },
+    { heading: "Lionel Messi's Barcelona teammate explains why superstar is not good example to follow", subtitle: "Mirror.co" },
+    { heading: "BOOK: BRIGHT YOUNG WOMEN ", subtitle: "Jessica Knoll" },
+    { heading: "BOOK: A Season on the Brink", subtitle: "John Feinstein" },
+    { heading: "Loose Balls", subtitle: "Johns carbora" },
+    { heading: "BOOK: TREMOR", subtitle: "Teju Cole" },
+    { heading: "BOOK :THE HALT DURING THE CHASE ", subtitle: "Rosemary Tonks" },
   ];
 
-  // Function to get a random subset of news articles
+
   const getRandomNewsArticles = (count) => {
-    const shuffledArticles = newsArticles.sort(() => 0.5 - Math.random()); // Shuffle the array
-    return shuffledArticles.slice(0, count); // Get the first 'count' articles
+    const shuffledArticles = newsArticles.sort(() => 0.5 - Math.random());
+    return shuffledArticles.slice(0, count); 
   };
 
-  const randomNewsArticles = getRandomNewsArticles(5); // Get 5 random news articles
+  const randomNewsArticles = getRandomNewsArticles(4);
 
   const newsArticle = (heading, subtitle) => (
     <div className="widgets-article">
